76da1f0b0f6a455d833a3fcf010b7a289bef9d99
Shomrey <szachym1@gmail.com>
2019-03-16 19:52:58 +0100

    final commit

--- App.js
+++ App.js
@@ -5,39 +5,24 @@ import Svg,{Polyline,} from 'react-native-svg';
 
 export default class MagnetometerSensor extends React.Component {
   state = {
-    magnetometerData: { x: 0, y: 0, z: 0 },
-    accelerometerData: { x: 0, y: 0, z: 0 },
-    gyroscopeData: { x: 0, y: 0, z: 0 },
-    barWidth: 300,
-    barHeight: 50, 
-    flexWidth: 30,
-  };
+    isFlat: false
+  }
 
   constructor(props) {
     super(props);
     this._scaleX = new Animated.Value(0);
     this._scaleY = new Animated.Value(0);
     this._rotateValue = new Animated.Value(0);
-    //this._translateX = this._animatedValue.interpolate({
-      //inputRange: [-1, 1],
-      //outputRange: [15, 269],
-      //extrapolate: 'clamp'
-    //});
-
     this._rotate = this._rotateValue.interpolate({
       inputRange: [0,360],
       outputRange: ['0deg', '360deg'],
       extrapolate: 'clamp'
     });
-
-    
   }
-
   
   componentDidMount() {
     Magnetometer.setUpdateInterval(50);
-    Gyroscope.setUpdateInterval(50);
-    Accelerometer.setUpdateInterval(300);
+    Accelerometer.setUpdateInterval(50);
     this._subscribe();
   }
 
@@ -47,16 +32,12 @@ export default class MagnetometerSensor extends React.Component {
 
   _subscribe = () => {
     this._subscriptions = [
-      Magnetometer.addListener(result => {
-        this.setState({ magnetometerData: result })
-        this._rotateValue.setValue(angle(result));
-      }),
-      Accelerometer.addListener(
-        result => {
-          this.setState({ accelerometerData: result });
-          this._scaleY.setValue(result.z / Math.sqrt(result.x * result.x + result.y * result.y + result.z * result.z ));
-      }),
-      Gyroscope.addListener(result => this.setState({ gyroscopeData: result }))
+      Magnetometer.addListener(result => this._rotateValue.setValue(angle(result))),
+      Accelerometer.addListener(result => {
+        let flatness = result.z / Math.sqrt(result.x * result.x + result.y * result.y + result.z * result.z);
+        this._scaleY.setValue(flatness);
+        this.setState({isFlat: flatness > 0.999})
+      })
     ];
   };
 
@@ -66,36 +47,16 @@ export default class MagnetometerSensor extends React.Component {
   };
 
   render() {
-    let m = this.state.magnetometerData;
-    let a = this.state.accelerometerData; 
-    let g = this.state.gyroscopeData;
-
+    let bgColor = this.state.isFlat ? "#378e88" : "#fff";
+    let logoColor = this.state.isFlat ? "#fff" : "#000";
     return (
-      <View style={styles.sensor}>
-        <Text>Magnetometer:</Text>
-        <Text>
-          xM: {round(m.x)} yM: {round(m.y)} zM: {round(m.z)}
-        </Text>
-        <Text>Gyroscope:</Text>
-        <Text>
-          xG: {round(g.x)} yG: {round(g.y)} zG: {round(g.z)}
-        </Text>
-        <Text>Accelerometer:</Text>
-        <Text>
-          xA: {round(a.x)} yA: {round(a.y)} zA: {round(a.z)}
-        </Text>
-
-        <Text>Angle: </Text>
-        <Text>
-          { round(angle(m)) }
-        </Text>
-
-        <Animated.View style = {[{transform: [{rotateZ: this._rotate}, /*{scaleX: this._scaleX}*/ {scaleY: this._scaleY}]}]}>
-          <Svg height="100%" width="100%" viewBox="0 0 1000 1000">
-            <Polyline fill="black" points="500 0, 933 250, 933 750, 824.75 812.5, 824.75 437.5, 500 625, 175.25 437.5, 175.25 500, 500 687.5, 770.625  531.25, 770.625 843.75, 500 1000, 67 750, 67 625, 500 875, 662.375 781.25, 662.375 718.75, 500 812.5, 67 562.5, 67 250"/>
+      <Animated.View style = {{backgroundColor: bgColor}}>
+        <Animated.View style = {{transform: [{rotateZ: this._rotate}, {scaleY: this._scaleY}]}}>
+          <Svg height="100%" width="100%" viewBox="0 0 1000 1000" rotation={180}>
+            <Polyline fill={logoColor} points="500 0, 933 250, 933 750, 824.75 812.5, 824.75 437.5, 500 625, 175.25 437.5, 175.25 500, 500 687.5, 770.625  531.25, 770.625 843.75, 500 1000, 67 750, 67 625, 500 875, 662.375 781.25, 662.375 718.75, 500 812.5, 67 562.5, 67 250"/>
           </Svg>
         </Animated.View>
-      </View>
+      </Animated.View>      
     );
   }
 }
@@ -108,47 +69,4 @@ function angle(m) {
   else if (m.y > 0) angle = -Math.atan(m.x/m.y) * 180 / 3.14159;
   else angle = 180 - Math.atan(m.x/m.y) * 180 / 3.14159;
   return 360 - ((angle > 0 ? angle : 360 + angle) + 5.61);
-}
-
-function round(n) {
-  if (!n) {
-    return 0;
-  }
-
-  return Math.floor(n * 100) / 100;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'stretch',
-    marginTop: 15,
-  },
-  bar: {
-    backgroundColor: 'green',
-  },
-  flex: {
-    position: 'absolute',
-    backgroundColor: 'red',
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-    padding: 10,
-  },
-  middleButton: {
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderColor: '#ccc',
-  },
-  sensor: {
-    marginTop: 15,
-    paddingHorizontal: 10,
-  },
-});
+} 
